38e9336e4d3f2fab094b74dba7ae6bdba99c8ab9
Jesús Pardillo <dev@jesuspardillo.com>
2013-08-18 01:21:18 +0200

    font installation

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
+# install config files
 REPOS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 CFG_FILES="vimrc gvimrc"
-
 for FILE in $CFG_FILES; do
    [[ -f $HOME/.$FILE ]] && mv -f $HOME/.$FILE  $HOME/.$FILE.LAST
    [[ -h $HOME/.$FILE ]] && unlink $HOME/.$FILE
    ln -s $REPOS_DIR/$FILE $HOME/.$FILE
 done
+
+# install fonts
+if [[ ! -f $HOME/.fonts/Inconsolata.otf ]]; then
+   URL="http://levien.com/type/myfonts/Inconsolata.otf"
+   mkdir -p $HOME/.fonts
+
+   cd $HOME/.fonts
+
+   wget --quite $URL 2> /dev/null || curl --silent --remote-name $URL
+   if [ $? -ne 0 ]; then
+      CODE=$LINENO;
+      echo "error:$CODE: wget/curl not found. Unable to download font Inconsolata.otf"
+      exit $CODE
+   fi
+
+   cd - > /dev/null
+fi
+
+echo "installation finished"
