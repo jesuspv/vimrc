c93b1f9021e8d7d7fed664c1539c590a004bc442
Jesús Pardillo <dev@jesuspardillo.com>
2013-08-18 17:17:32 +0200

    fixing unlinking previous installations

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 REPOS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CFG_FILES="vimrc gvimrc"
 for FILE in $CFG_FILES; do
-   [[ -f $HOME/.$FILE ]] && mv -f $HOME/.$FILE  $HOME/.$FILE.LAST
    [[ -h $HOME/.$FILE ]] && unlink $HOME/.$FILE
+   [[ -f $HOME/.$FILE ]] && mv -f $HOME/.$FILE $HOME/.$FILE.LAST
    ln -s $REPOS_DIR/$FILE $HOME/.$FILE
 done
 
